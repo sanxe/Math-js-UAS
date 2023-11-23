4b088d1015650b8a003cdbd7f458e099629d5a1e
sanxe <146511000+sanxe@users.noreply.github.com>
2023-11-23 10:30:03 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+// Math.round
+
+let roundnum1 = Math.round(3.14);
+console.log(roundnum1);
+
+let roundnum2 = Math.round(4.6);
+console.log(roundnum2);
+
+let roundnum3 = Math.round(1.2);
+console.log(roundnum3);
+
+// Math.floor
+
+let floornum1 = Math.floor(8.1231323);
+console.log(floornum1);
+
+let floornum2 = Math.floor(9.445);
+console.log(floornum2);
+
+let floornum3 = Math.floor(4.765645);
+console.log(floornum3);
+
+// Math.ceil
+
+let ceilnum1 = Math.ceil(1.9);
+console.log(ceilnum1);
+
+let ceilnum2 = Math.ceil(3.6);
+console.log(ceilnum2);
+
+let ceilnum3 = Math.ceil(2.8);
+console.log(ceilnum3);
+
+// Math.pow
+
+let pownum1 = Math.pow(4, 2);
+console.log(pownum1)
+
+let pownum2 = Math.pow(7, 2);
+console.log(pownum2)
+
+let pownum3 = Math.pow(5, 2);
+console.log(pownum3)
+
+// Math.sqrt
+
+let rootnum1 = Math.sqrt(16);
+console.log(rootnum1);
+
+let rootnum2 = Math.sqrt(49);
+console.log(rootnum2);
+
+let rootnum3 = Math.sqrt(25);
+console.log(rootnum3);
